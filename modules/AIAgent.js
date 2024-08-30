c3fcaa51dab4838803d651c1010b1c7a15ad2365
Matthew Rae <mattyjrae123@hotmail.com>
2024-08-30 18:22:52 +1200

    Add terminal states to minimax function

--- modules/AIAgent.js
+++ modules/AIAgent.js
@@ -8,6 +8,22 @@ const AIAgent = (() => {
   const getMove = (board) => {
     return board.getPossibleMoves()[0];
   };
+
+  const _minimax = (board, isMaximisingPlayer) => {
+    // terminal state 1
+    if (board.gameWon()) {
+      if (isMaximisingPlayer) {
+        return -1;
+      }
+
+      return 1;
+    }
+
+    // terminal state 2
+    if (!board.hasMovesRemaining()) {
+      return 0;
+    }
+  };
   
   return {
     getMove
